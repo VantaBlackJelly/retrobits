0e06a5eea25ddd0e9a91b982d95f4e127d84641f
Mateo Stankewitz <mateostankewitz@gmail.com>
2024-03-22 03:46:42 -0700

    Initial commit, got svgdom and svg.js to play nice with node

---
+++ app.js
@@ -0,0 +1,63 @@
+//HTTP handlers
+import express from 'express'
+import axios from 'axios'
+
+const app = express()
+const PORT = 3000
+
+//Image / SVG manipulation
+const sharp = import('sharp')
+import { createSVGWindow } from 'svgdom'
+import { SVG, registerWindow } from '@svgdotjs/svg.js'
+
+//      Async / Await example because I can never remember how to do it
+//________________________________________________________________________
+//Define an asynchronous function to be called upon a homepage GET request
+// async function getRandomActivity(){
+
+//     try{
+
+//         //Store response from api promise in variable
+//         let response = await axios.get('http://www.boredapi.com/api/activity/')
+        
+//         //Return for use in higher scope
+//         return response.data
+
+//     }
+//     catch(err){
+
+//         console.log(err)
+
+//     }
+
+// }
+//________________________________________________________________________
+
+
+// returns a window with a document and an svg root node
+const window = createSVGWindow()
+const document = window.document
+
+// register window and document
+registerWindow(window, document)
+
+const canvas = SVG(document.documentElement)
+
+canvas.rect(100, 100).fill('yellow').move(50,50)
+
+//Homepage 
+app.get('/', (req, res) => 
+    {
+        try {
+                console.log(`${res.statusCode}`)
+                res.send(canvas.svg())
+            } 
+        catch (error) {
+                console.log(error)
+                res.sendStatus(500)
+            }
+        }
+)
+
+//Start web server on desinated PORT
+app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`))
