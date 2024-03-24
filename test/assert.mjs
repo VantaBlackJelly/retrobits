f51a880f4c3ed5a705c856f35dd92a28107511b9
Mateo Stankewitz <mateostankewitz@gmail.com>
2024-03-23 23:00:05 -0700

    added method to get bit ratio, consider whether this needs to be in global scope of module vs within imageMatrix class definition

--- test/assert.mjs
+++ test/assert.mjs
@@ -1,10 +1,12 @@
 
 import imageMatrix from '../lib/image-encode.mjs'
 
-
 console.log(process.argv[2])
 
 const grid = new imageMatrix(process.argv[2],8)
 
-grid.getMetaData(["width","height","depth","peepee"],true)
-    .then((data)=>{console.log(data)})
+grid.getMetaData(["width", "height"],true)
+    .then((data)=>{
+        let [w,h] = grid.getBitRatio(data["width"],data["height"]);
+        console.log(`\n Aspect ratio of image is: ${w}/${h}\n`)
+    })
